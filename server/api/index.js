82e3913e23bb7529959aea712ef0d44d12f626cb
thangph9 <root@localhost.localdomain>
2019-03-19 14:18:59 +0700

    add index members

--- server/api/index.js
+++ server/api/index.js
@@ -8,4 +8,5 @@ router.use('/upload', require('./upload'));
 router.use('/track', require('./track'));
 router.use('/question', require('./question'));
 router.use('/group', require('./group'));
+router.use('/members', require('./members'));
 module.exports = router;
