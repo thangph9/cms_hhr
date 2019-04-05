7c34717b9b0020293ca5bab380b5b111ded720f9
thangph9 <root@localhost.localdomain>
2019-04-05 11:01:18 +0700

    update users model

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -2,29 +2,42 @@ module.exports = {
   fields: {
     user_id: 'uuid',
     address: 'text',
+    audio: {
+      type: 'map',
+      typeDef: '<text,uuid>',
+    },
     avatar: 'uuid',
     country: 'text',
+    createat: 'timestamp',
     description: 'text',
     distance: 'float',
     dob_day: 'int',
     dob_month: 'int',
     dob_year: 'int',
+    education: {
+      type: 'map',
+      typeDef: '<text,text>',
+    },
     email: 'text',
     fullname: 'text',
-    gender: 'varchar',
+    gender: 'text',
     height: 'text',
     hhr_goal: 'text',
+    jobs: {
+      type: 'map',
+      typeDef: '<text,text>',
+    },
     phone: 'text',
+    phones: {
+      type: 'map',
+      typeDef: '<text,text>',
+    },
     uniqueid: 'int',
     video: {
       type: 'map',
       typeDef: '<text,uuid>',
     },
-    audio: {
-      type: 'map',
-      typeDef: '<text,uuid>',
-    },
-    createat: 'timestamp',
+    weight: 'text',
   },
   key: ['user_id'],
 };
