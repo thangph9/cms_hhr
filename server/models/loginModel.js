ce9ac42d296ade543a60c02ad4f70e39d62fae18
root <root@instance-1.asia-east2-a.c.henhoradio-237202.internal>
2019-04-11 02:45:44 +0000

    update login models

--- server/models/loginModel.js
+++ server/models/loginModel.js
@@ -2,6 +2,10 @@ module.exports = {
   fields: {
     phone: 'text', // unicode
     user_id: 'uuid', // 128bit
+    rule: {
+      type: 'set',
+      typeDef: '<text>',
+    },
     password: 'text',
     password_hash_algorithm: 'text',
     password_salt: 'text',
