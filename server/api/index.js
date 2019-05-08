17d03d933cb818bbf382a3fd1014678ebf8c88d4
thangph9 <root@localhost.localdomain>
2019-05-08 11:16:54 +0700

    update service

--- server/api/index.js
+++ server/api/index.js
@@ -4,12 +4,13 @@
 var express = require('express');
 var router = express.Router();
 router.use('/user', require('./user'));
-router.use('/upload', require('./upload'));
 router.use('/track', require('./track'));
 router.use('/question', require('./question'));
 router.use('/group', require('./group'));
 router.use('/members', require('./members'));
 router.use('/menu', require('./menu'));
 router.use('/menuItem', require('./menuItem'));
+router.use('/auth', require('./authentication'));
 router.use('/rule', require('./rule'));
+router.use('/services', require('./services'));
 module.exports = router;
