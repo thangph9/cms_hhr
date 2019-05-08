89fed5951cbf459839d873c4678f4d76bde9adcc
thangph9 <root@localhost.localdomain>
2019-05-08 12:11:33 +0700

    update service register

--- server/api/services.js
+++ server/api/services.js
@@ -1,14 +1,77 @@
 const async = require('async'); // eslint-disable-line
 const express = require('express'); // eslint-disable-line
-// const models = require('../settings');
+const models = require('../settings');
 
-// const Uuid = models.datatypes.Uuid; // eslint-disable-line
+const Uuid = models.datatypes.Uuid; // eslint-disable-line
 
 const router = express.Router();
+/*
+  service_manager_id: '14',
+  command_code: 'HHR',
+  user_id: '84963760289',
+  service_id: '8779',
+  message: 'HHR Thang Nam',
+  request_id: '4931246570' 
+*/
 
-function save(req, res) {
-  console.log(req.headers);
-  console.log(req.body);
+function service(req, res) {
+  const PARAM_IS_VALID = {};
+  const params = req.body;
+  async.series(
+    [
+      function initParams(callback) {
+        try {
+          PARAM_IS_VALID.id = Uuid.random();
+          PARAM_IS_VALID.command_code = params.command_code;
+          PARAM_IS_VALID.user_id = params.user_id;
+          PARAM_IS_VALID.service_id = params.service_id;
+          PARAM_IS_VALID.message = params.message;
+          PARAM_IS_VALID.request_id = params.request_id;
+        } catch (e) {
+          console.log(e);
+        }
+        callback(null, null);
+      },
+      function saveServiceRegister(callback) {
+        const serviceRegister = {
+          id: PARAM_IS_VALID.id,
+          command_code: PARAM_IS_VALID.command_code,
+          user_id: PARAM_IS_VALID.user_id,
+          service_id: PARAM_IS_VALID.service_id,
+          message: PARAM_IS_VALID.message,
+          request_id: PARAM_IS_VALID.request_id,
+        };
+        const instance = new models.instance.serviceRegister(serviceRegister); // eslint-disable-line
+        instance.save(err => {
+          callback(err);
+        });
+      },
+    ],
+    err => {
+      console.log(err);
+      const data = {
+        status: '0',
+        request_id: PARAM_IS_VALID.request_id,
+        sms: [
+          {
+            message: 'Hello Test Service',
+            message_type: '1',
+            total_message: '1',
+            message_index: '1',
+            content_type: '0',
+            is_more: '0',
+          },
+        ],
+      };
+      if (err) res.send({ ...data });
+      else res.json({ ...data });
+    }
+  );
+}
+
+function otp(req, res) {
+  const params = req.body;
+  console.log(params);
   const data = {
     status: '0',
     request_id: '10000',
@@ -23,9 +86,11 @@ function save(req, res) {
       },
     ],
   };
-  console.log(data);
   res.json({ ...data });
 }
-router.get('/register', save);
-router.post('/register', save);
+
+// router.get('/register', save);
+router.post('/register', service);
+router.post('/otp', otp);
+
 module.exports = router;
---
+++ server/models/serviceRegisterModel.js
@@ -0,0 +1,49 @@
+module.exports = {
+  fields: {
+    id: {
+      type: 'uuid',
+      default: { $db_function: 'uuid()' },
+    },
+    command_code: 'text',
+    request_id: 'text',
+    message: 'text',
+    user_id: 'text',
+    service_id: 'text',
+    year: {
+      type: 'int',
+      default: parseInt(new Date().getFullYear()), // eslint-disable-line
+    },
+    created: {
+      type: 'timestamp',
+      default: { $db_function: 'toTimestamp(now())' },
+    },
+  },
+  key: [['id'], 'created'],
+  clustering_order: { created: 'desc' },
+  materialized_views: {
+    view_group2: {
+      select: [
+        'command_code',
+        'request_id',
+        'message',
+        'user_id',
+        'service_id',
+        'id',
+        'created',
+        'year',
+      ],
+      key: [['year', 'id'], 'created'],
+      clustering_order: { created: 'desc' },
+    },
+  },
+  indexes: ['command_code'],
+  options: {
+    timestamps: {
+      createdAt: 'created_at', // defaults to createdAt
+      updatedAt: 'updated_at', // defaults to updatedAt
+    },
+    versions: {
+      key: '__v1', // defaults to __v
+    },
+  },
+};
